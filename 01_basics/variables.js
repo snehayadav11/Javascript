bdadc9d8f01392a41306bae52a644476ea50d452
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-10-15 17:38:10 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "snehay335@gmail.com"
+let accountPassword = "sneha"
+accountCity = "Ahiwara"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
