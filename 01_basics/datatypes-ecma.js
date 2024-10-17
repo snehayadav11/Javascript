4978373f16a4430e70de9e382de6c7ce44834539
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-10-17 14:25:51 +0000

    container config done

---
+++ 01_basics/datatypes-ecma.js
@@ -0,0 +1,28 @@
+// ⭐ "use strict"; // treats all js code as a newer version
+
+//  ⭐ alert("hello") // will not pop up as we are using node not browser
+
+console.log(3 
+    + 3
+)            // code readability should be maintained 
+
+// tc39.es is the website for documentation and u are aware of mdn docs
+
+/* ⭐ datatypes ⭐
+
+null (standalone value) - empty values let money = null;---> my own
+undefined - let state;
+symbol = unique will understand this while doing react
+object
+
+*/ 
+
+// 🚨🚨🚨  in terms of interview 🚨🚨🚨
+
+//console.log(typeof 3)
+
+console.log(typeof null)     // null itself is not a datatype itsan object ---> 0/p --> object
+
+console.log(typeof undefined) // but undefined is itselff a datatype      ---> 0/p --> undefined
+
+
