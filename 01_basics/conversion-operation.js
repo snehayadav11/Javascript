dd1a67ff406da69db303444be0509f2075b83ee9
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-10-17 16:01:34 +0000

    conversion of datatypes

---
+++ 01_basics/conversion-operation.js
@@ -0,0 +1,65 @@
+
+/*  ➡️ typeof can be used in two ways ⬇️
+
+1. console.log(typeof null)
+
+2. console.log(typeof (null))
+
+*/
+
+let score = "22abc"   
+//console.log(typeof score)
+
+//score = "22"
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+//score = "22abc"
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber) // o/p --> NaN that meANS it is not a number;
+
+/*
+33 --> = number (33)
+22abc --> NaN
+true --> 1 / false --> 0
+null --> 0 / NaN 
+undefined --> Nan 
+                  lekin NaN ka datatype no hi h kuki yahan convert Number m kara rahe h
+*/
+
+/* let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn)
+
+ OUTPUT = true
+
+ */
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) 
+
+// OUTPUT = FALSE
+
+let isLoggedIn = "Sneha"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn) 
+
+// OUTPUT = TRUE
+
+/*
+
+1 => true , 0 => false
+""=> false , "sneha"=> true
+
+*/
+
+
+
+
+
