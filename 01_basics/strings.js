d3a9605df3d4ac09d333aa37a7270a8be381d037
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-11-19 14:48:55 +0000

    string-methods

---
+++ 01_basics/strings.js
@@ -0,0 +1,40 @@
+/*
+
+const firstName = "Sneha" 
+const secondName = "Yadav" 
+
+console.log( firstName + secondName ) ; // this is an older version of adding strings
+
+console.log(`My name is ${firstName} ${secondName}`);
+
+*/
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨  STRINGS CAN ALSO BE DEFINED AS
+
+//const name = new String (' Sneha ') 
+
+// in strings jo b char store hote h vo array k form p nahi hote h blki objects k form m hote h that means key value pairs m
+
+// strings k kafi sare functions hote h
+// or unko hi pdhna strings ko smjna kehlata h
+
+// strings some functions like .length , toUppercase , charat(2) ;
+
+//🚨🚨🚨🚨 slice m negative m bhi de skte h index while not in substring
+
+// strings replace funtion is so useful
+
+let name = 'crazy';
+console.log(name.replace('crazy','cute'));
+
+// strings functions or say methods are really imp so do read mdn docs
+
+
+
+
+
+
+
+
