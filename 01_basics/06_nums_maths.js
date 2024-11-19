5ce635b5072e9214242efbed608181e1a05a73f3
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-11-19 16:25:16 +0000

    number and maths

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,22 @@
+/*
+const score = 400
+console.log(score); 
+
+const newScore = new Number(100)
+console.log(newScore);
+
+slight changes in there output in one we purposely define then it should be a number
+
+*/
+
+// in number do prefer mdn docs on certaion toppics such as toLocaleString() , tofixed() , toprecision() ; 
+ 
+const min = 10
+const max = 20 
+
+// Math.random()always gives no betwwn zero and 1
+console.log(Math.random());
+console.log(Math.floor(Math.random()));
+
+
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min) ;
