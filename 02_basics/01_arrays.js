a290958e80d18e71b75845245b7bc9f9f21d30e5
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-11-25 12:22:07 +0000

    array basics in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+const myArr = [ 1 , 2 , 3 , 4 , 5 ]
+
+//  const arr = [ "sneha" , " anuu " ]
+
+//const arr2 = new Array( 1 , 2 , 3 , 4 , 5) ; 
+
+//console.log(arr2[1]);
+
+// browser inspect array krne s ek print krane s so many prototype aate h so go through that tooo;
+
+// 🚨🚨 ARRAY METHODS 🚨🚨
+
+// arr2.push(22)
+// console.log(arr2);
+
+// arr2.pop()
+// console.log(arr2);
+
+// arr2.unshift(22) // puts the bracket element into first index but its not the optimsed way as each element in array got shifted
+// console.log(arr2);
+
+// arr2.shift() // Removes the first element from an array and returns it. If the array is empty, 
+// console.log(arr2);                                                     undefined is returned and the array is not modified.
+
+//console.log(arr2.includes(9)); // gives boolean values whether an element exists or not in the array
+//console.log(arr2.indexOf(9));// if an element is not present returns -1 or else returns the index of the element present
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr); // both will be same but the newArr will be string because of join
+
+// console.log(typeof newArr);
+
+// TWO IMPORTANT FUNCTION OF ARRAYS 
+
+// 🚨🚨SLICE AND SPLICE🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
+
+// in slice original array is not manipulated and if range is from (1,3) then the last range is not included
+
+// in splice from original array the given range is removed which means original array is manipulated and also the lasr range is also 
+//           included which means removedd
