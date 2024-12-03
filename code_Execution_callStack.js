972aa2fefa74def1dade34d67a57d2d2b9f2c8da
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-12-03 10:39:41 +0000

    CODE EXECUTION AND  CALL STACK IN JS

---
+++ code_Execution_callStack.js
@@ -0,0 +1,24 @@
+
+// +++++++++++++++++++++ JAVASCRIPT EXECUTION CONTEXT ++++++++++++++++++++++++++++
+// meaning jo b file banaya h vo run kese hoga javascript m 
+
+//                                                    1- GLOBAL EXECUTION CONTEXT
+//                                                    2- FUNCTIONAL EXECUTION CONTEXT
+//--> property of global object basically             3- EVAL EXECUTION CONTEXT (NOT THAT IMP)
+
+
+// jo b code file hota h usme hota h 🚨🚨🚨 GLOBAL EXECUTION CONTEXT 🚨🚨🚨 usko THIS m store kara diya jata h or browser ka this( mtlb global EC ) m window hota h 
+// javascript single threaded h yaha har chiz process h
+
+// programs by javascript are run in two phase ----> 1- MEMORY CREATION PHASE also known as(Creation phase)
+//                                                   2- EXECUTION PHASE 
+
+// MEMORY EXECUTION PHASE M JO B VARIABLES , YA MATHEMATICAL COMPUTATION HOTE H + , - USKE LIYE MEMORY ALLOCATE HOTA H
+// OR ALL EXECUTION PHASE M EXECUTION HOTA H SARA 
+
+// BETTER UNDERSTAND IT WITH THE DRAWING 
+
+
+
+// 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
+// U CAN LEARN ABOUT CALLSTACK IN BROWSER --> SOURCES ---> CALLL STACK  
