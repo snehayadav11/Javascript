a79bdb1aa5e89d1a43b8328cb3b489d95961f69b
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-12-04 09:17:24 +0000

    switch in js

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+// SWITCH CASE IN JAVASCRIPT
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;                      // break lagana isliye jruri h kuki agar ye true hogya cae toh iske bas s pure case print honge 
+                                    // bina unke condition check kiye lekin MUST REMEMBER DEFAULT CAE N HOGA PRINT obvious baat h na kuki 
+                                    // vo toh tab execute hota h jab koi case match n hota h
+
+                                    //to duplicate shift + alt + down arrow
+
+    case 2:
+        console.log("february");
+        break;
+        
+    case 3:
+        console.log("March");
+        break;
+
+    case 4:
+        console.log("April");
+        break;
+        
+        
+
+
+    default:
+        console.log("hello")
+        break;
+}
