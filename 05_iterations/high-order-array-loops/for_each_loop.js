c997b0da7bab1a5e8315f3c080fe584902dbddd7
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-12-08 07:19:56 +0000

    fOR - EACH - LOOP IN JS

--- 05_iterations/high-order-array-loops/for_each_loop.js
+++ 05_iterations/high-order-array-loops/for_each_loop.js
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////// FOR EACH LOOP /////////////////////////////////////////////////////
 
-const arr = [1 , 2 ,3 ,4 ,5 ]
+// const arr = [1 , 2 ,3 ,4 ,5 ]
 
 //FOR EACH LOOP
  
@@ -18,17 +18,17 @@ const arr = [1 , 2 ,3 ,4 ,5 ]
 
 // function bana k bhi pass kr skte h
 
-function printMe(item){
+//function printMe(item){
     //console.log(item);
-}
-arr.forEach(printMe) // printMe() aise pass n krna h kuki uska refrence dena h yahan naki execute krna h 
+//}
+//arr.forEach(printMe) // printMe() aise pass n krna h kuki uska refrence dena h yahan naki execute krna h 
 
 ////////////// for each k andar sirf ek item hi n pass krskte h blki kafi kuch pass kr skte h lets see
 
-arr.forEach((item , index , arr) => {
+//arr.forEach((item , index , arr) => {
     //console.log(item , index , arr);
 
-})
+// })
 /*
 1 0 [ 1, 2, 3, 4, 5 ]
 2 1 [ 1, 2, 3, 4, 5 ]
@@ -37,23 +37,48 @@ arr.forEach((item , index , arr) => {
 5 4 [ 1, 2, 3, 4, 5 ]
 */ 
 
+
+
+
 // lets discuss this one [{},{},{}]
 
-const myCoding = [
-    {
-        languageName : "Javascript",
-        languageFile : "js"
-    },
-    {
-        languageName : "Java",
-        languageFile : "java"
-    },
-    {
-        languageName : "CPP",
-        languageFile : "C++"
-    }
-]
-myCoding.forEach( (item) => {
-    console.log(item.languageName);
+// const myCoding = [
+//     {
+//         languageName : "Javascript",
+//         languageFile : "js"
+//     },
+//     {
+//         languageName : "Java",
+//         languageFile : "java"
+//     },
+//     {
+//         languageName : "CPP",
+//         languageFile : "C++"
+//     }
+// ]
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName);
     
+// })
+
+
+
+
+
+//🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
+// lets see that for-each loop returns value or not
+
+
+const arr = [1 , 2 ,3 ,4 ,5 ]
+const values = arr.forEach( (item) =>{
+    //console.log(item);
+    return item;
 })
+console.log(values) // --- > undefined
+
+
+// for each loop do not return any value  as the above example shows , the operations in which we have to perform that if = this return we cant use this method
+//🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
+
+
+//// TO SOLVE THE ABOVE PROBLM OF RETURNIN THE VALUE LETS DISCUSS ANOTHER METHOD WHICH IS FILTER
