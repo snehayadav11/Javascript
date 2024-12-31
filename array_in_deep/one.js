ca377fd235dc39ade15d9998079bdfb65c97fdef
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2024-12-31 15:43:34 +0000

    learnt about packed and holey array in js

---
+++ array_in_deep/one.js
@@ -0,0 +1,12 @@
+const myArr = [] // empty array
+%DebugPrint(myArr)
+
+//  DebugPrint is not defined 
+// this is the O/P got from the second line
+
+// yeh error isliye aaya kuki vs cod em jo html css ye sb hota h v kuch chizo k liye restricted h vo n ho skta vs code m toh 
+// toh hum isko dekhenge jV8 engine me github k source code m v8
+
+// search github v8 on google
+
+// see your notes in copy of js about two types of aray which is packed also known as continuous and holey array
